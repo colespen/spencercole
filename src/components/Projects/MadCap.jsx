f72c6a38b9cdad2a278ada916719ff13eef6add1
colespen <spencerlewiscole@gmail.com>
2023-01-09 15:34:20 -0800

    setup visual mode transition between project pages in modal

--- src/components/Projects/MadCap.jsx
+++ src/components/Projects/MadCap.jsx
@@ -2,25 +2,23 @@ import { Fragment } from 'react';
 import './Projects.scss';
 
 
-export default function ShowFinder() {
-
-
+export default function MadCap() {
 
 
   return (
     <Fragment>
 
       <header>
-        <h1>Project1</h1>
+        <h1>Project2</h1>
       </header>
 
       <main>
-        <p>this is a project</p>
+        <p>this is another project</p>
         <br></br>
         <p>the stack is this and this and this</p>
         <p>and this as well.</p>
         <br></br>
-        <p>check it out here.</p>
+        <p>check it out herreeee.</p>
       </main>
     </Fragment>
   );
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import classNames from 'classnames';
-import '../Projects.scss';
+
+import ShowFinder from './ShowFinder';
+import MadCap from './MadCap';
+
+import './Projects.scss';
 
 
 export default function Projects({ show, handleShowHideWindow }) {
@@ -10,33 +15,45 @@ export default function Projects({ show, handleShowHideWindow }) {
     "display-none": show,
   });
 
+  function useVisualMode(initial) {
+    const [page, setPage] = useState(initial);
+
+    const transition = nextmode => {
+      if (nextmode) setPage(nextmode);
+      else setPage(initial);
+    };
+    return { mode: page, transition };
+  }
+
+  const { mode, transition } = useVisualMode("P1")
+
+  const handleProjectBtnClick = (e) => {
+    transition(e.currentTarget.id);
+  }
 
 
   return (
     <div className={windowShowHide}>
       <section className="window-main">
         <nav className="projects-nav">
-          <button>project1</button>
-          <button>project2</button>
-          <button>project3</button>
-          <button>project4</button>
+          <button id="P1" onClick={handleProjectBtnClick}
+          >project1</button>
+          <button id="P2" onClick={handleProjectBtnClick}
+          >project2</button>
+          <button id="P3" onClick={handleProjectBtnClick}
+          >project3</button>
+          <button id="P4" onClick={handleProjectBtnClick}
+          >project4</button>
         </nav>
-          <button className="close-btn" onClick={handleShowHideWindow}>
-            <img src="./close.png" alt="close-window"/>
-            </button>
+        <button className="close-btn" onClick={handleShowHideWindow}>
+          <img src="./close.png" alt="close-window" />
+        </button>
 
-        <header>
-          <h1>Project1</h1>
-        </header>
+        <div className="inner-window">
+          { mode === "P1" && <ShowFinder />}
+          { mode === "P2" && <MadCap />}
+        </div>
 
-        <main>
-          <p>this is a project</p>
-          <br></br>
-          <p>the stack is this and this and this</p>
-          <p>and this as well.</p>
-          <br></br>
-          <p>check it out here.</p>
-        </main>
       </section>
     </div>
   );
