2720c33ebef91f6b7af4fb85598d9bc972965830
colespen <spencerlewiscole@gmail.com>
2023-01-08 19:08:39 -0800

    create react app

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div className="title-container">
+      <h1 className="title">Spencer Cole</h1>
+    </div>
+  );
+}
+
+export default App;
