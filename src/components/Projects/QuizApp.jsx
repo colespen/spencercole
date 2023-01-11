7a5019ca9903ab9c887f4af006d719f1bb863522
colespen <spencerlewiscole@gmail.com>
2023-01-10 19:10:28 -0800

    make carousel for media in projects

--- src/components/Projects/QuizApp.jsx
+++ src/components/Projects/QuizApp.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 
 import '../modal.scss';
 
+import Carousel from './Carousel';
 
 export default function QuizApp() {
 
@@ -13,8 +14,14 @@ export default function QuizApp() {
         <h1>QuizApp</h1>
       </header>
 
-      <img className="project-icon" src="./quizapp1.png" 
-      style={{borderRadius: '30px', height: '320px'}}/>
+      <Carousel>
+        <img className="project-screenshot" src="./quizapp1.png" alt="screenshot1"/>
+        <img className="project-screenshot" src="./quizapp1a.png" alt="screenshot2"/>
+        <img className="project-screenshot" src="./quizapp2.png" alt="screenshot3"/>
+        <img className="project-screenshot" src="./quizapp3.png" alt="screenshot4"/>
+        <img className="project-screenshot" src="./quizapp4.png" alt="screenshot5"/>
+        <img className="project-screenshot" src="./quizapp5.png" alt="screenshot6"/>
+      </Carousel>
 
       <main className="tab-description">
         <p>this is another project</p>
