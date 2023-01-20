70ceec0df3dc9669b6fcebe1a3b149f63383673a
colespen <spencerlewiscole@gmail.com>
2023-01-20 18:18:31 -0500

    Make background sick

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from 'react';
+import { useState, Fragment, useEffect } from 'react';
 import classNames from 'classnames';
 
 import './styles.scss';
@@ -15,29 +15,15 @@ import useVisualMode from '../hooks/useVisualMode';
 export default function Main() {
 
   const [show, setShow] = useState(false);
+
   const [isPointerEvent, setIsPointerEvent] = useState("auto");
   const [isDrawClear, setIsDrawClear] = useState(false);
 
   //// For Modal Stain Transition
+  const [isStainVisible, setIsStainVisible] = useState("hidden");
   const [flash, setFlash] = useState(0);
   const [border, setBorder] = useState(0);
   const [clicks, setClicks] = useState(0);
-
-  const handleShowHideWindow = e => {
-    setClicks(prev => prev + 1);
-    if (!show) {
-      setShow(true);
-      setBorder(1);
-      if (clicks % 2 === 0 && clicks !== 0) {
-        setFlash(1);
-      }
-    } else {
-      setShow(false);
-      setBorder(0);
-      setFlash(0);
-    }
-    transition(e.currentTarget.id, null);
-  };
 
   const { view, transition } = useVisualMode("home", null);
 
@@ -47,9 +33,28 @@ export default function Main() {
     "display-none": !show,
   });
 
+  const handleShowHideWindow = e => {
+    setClicks(prev => prev + 1);
+    if (!show) {
+      setShow(true);
+      (clicks % 6 === 0 && clicks !== 0) ?
+        setFlash(1) : setBorder(1);
+    } else {
+      setShow(false);
+      setBorder(0);
+      setFlash(0);
+    }
+    transition(e.currentTarget.id, null);
+  };
+
   const handleClearDraw = () => {
     !isDrawClear ? setIsDrawClear(true) : setIsDrawClear(false);
   };
+
+  useEffect(() => {
+    view.page === "home" ?
+      setIsStainVisible("visible") : setIsStainVisible("hidden");
+  }, [view.page]);
 
   return (
 
@@ -67,13 +72,13 @@ export default function Main() {
         />}
 
       <section className="modal-stain light-border"
-        style={{ opacity: border }}
+        style={{ opacity: border, visibility: isStainVisible }}
       ></section>
       <section className="modal-stain flash"
-        style={{ opacity: flash }}
+        style={{ opacity: flash, visibility: isStainVisible }}
       ></section>
       <section className="modal-stain heavy-border"
-        style={{ opacity: flash }}
+        style={{ opacity: flash, visibility: isStainVisible }}
       ></section>
 
       <div className={windowShowHide}>
